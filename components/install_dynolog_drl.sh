373398dd4157a402e922cd501fc8e1b9a669c4c7
Jason Taylor <jason.taylor.jt@microsoft.com>
2026-03-13 21:03:02 +0000

    added git branch version

--- components/install_dynolog_drl.sh
+++ components/install_dynolog_drl.sh
@@ -8,6 +8,7 @@ source ${UTILS_DIR}/utilities.sh
 
 if [[ "$GPU" == "NVIDIA" ]]; then
 
+    ORIGINAL_PATH="$PATH"
     DYNOLOG_INSTALL_DIR=/opt/dynolog/bin
     mkdir -p $DYNOLOG_INSTALL_DIR
 
@@ -20,21 +21,55 @@ if [[ "$GPU" == "NVIDIA" ]]; then
     DRL_URL=$(jq -r '.url' <<< $drl_metadata)
 
     ##########################################################################
-    # Install build dependencies
+    # Install build dependencies, tracking newly installed packages
     ##########################################################################
+    NEWLY_INSTALLED_PKGS=()
+
+    pkg_is_installed() {
+        local pkg="$1"
+        if command -v dpkg-query &>/dev/null; then
+            dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "install ok installed"
+        elif command -v rpm &>/dev/null; then
+            rpm -q "$pkg" &>/dev/null
+        fi
+    }
+
+    install_and_track() {
+        local manager="$1"
+        shift
+        local to_install=()
+        for pkg in "$@"; do
+            if pkg_is_installed "$pkg"; then
+                echo "Package '$pkg' is already installed, skipping"
+            else
+                to_install+=("$pkg")
+            fi
+        done
+        if [[ ${#to_install[@]} -gt 0 ]]; then
+            $manager install -y "${to_install[@]}"
+            NEWLY_INSTALLED_PKGS+=("${to_install[@]}")
+        fi
+    }
+
     if [[ $DISTRIBUTION == "azurelinux3.0" ]]; then
-        tdnf install -y cmake cargo ninja-build build-essential rust
+        install_and_track tdnf cmake cargo ninja-build build-essential
     elif [[ $DISTRIBUTION == *"ubuntu"* ]]; then
-        apt install -y cmake cargo ninja-build build-essential rustc
-        apt install -y g++ pkg-config uuid-dev libssl-dev
+        install_and_track apt-get cmake cargo ninja-build build-essential
+        install_and_track apt-get g++ pkg-config uuid-dev libssl-dev
     elif [[ $DISTRIBUTION == almalinux* ]] || [[ $DISTRIBUTION == rocky* ]]; then
-        yum install -y cmake cargo ninja-build rust-toolset
+        install_and_track yum cmake cargo ninja-build
     fi
+
+    export RUSTUP_HOME=/tmp/cargo-rust
+    export CARGO_HOME=/tmp/cargo-rust
+    export RUSTUP_INIT_SKIP_PATH_CHECK=yes
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    export PATH="$RUSTUP_HOME/bin:$PATH"
 
     ##########################################################################
     # Build and install dynolog
     ##########################################################################
-    git clone --recurse-submodules -j8 $DYNOLOG_URL /tmp/dynolog
+    git clone --recurse-submodules -j8 --branch v${DYNOLOG_VERSION}  $DYNOLOG_URL /tmp/dynolog
     pushd /tmp/dynolog
     ./scripts/build.sh -DCMAKE_POLICY_VERSION_MINIMUM=3.5
     mv build/dynolog/src/dynolog $DYNOLOG_INSTALL_DIR
@@ -66,7 +101,7 @@ EOF
     ##########################################################################
     # Build and install dyno-relay-logger
     ##########################################################################
-    git clone --recurse-submodules -j8 $DRL_URL /tmp/dyno-relay-logger
+    git clone --recurse-submodules -j8 --branch v${DRL_VERSION} $DRL_URL /tmp/dyno-relay-logger
     pushd /tmp/dyno-relay-logger
     mkdir build && cd build
     cmake .. -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DCMAKE_BUILD_TYPE=Release
@@ -96,6 +131,24 @@ EOF
 
     systemctl daemon-reload
     systemctl enable dyno-relay-logger.service
+
+    ##########################################################################
+    # Remove build dependencies that were not originally installed
+    ##########################################################################
+    if [[ ${#NEWLY_INSTALLED_PKGS[@]} -gt 0 ]]; then
+        echo "Removing newly installed build dependencies: ${NEWLY_INSTALLED_PKGS[*]}"
+        if [[ $DISTRIBUTION == "azurelinux3.0" ]]; then
+            tdnf remove -y "${NEWLY_INSTALLED_PKGS[@]}" || true
+        elif [[ $DISTRIBUTION == *"ubuntu"* ]]; then
+            apt-get remove -y "${NEWLY_INSTALLED_PKGS[@]}" || true
+            apt-get autoremove -y || true
+        elif [[ $DISTRIBUTION == almalinux* ]] || [[ $DISTRIBUTION == rocky* ]]; then
+            yum remove -y "${NEWLY_INSTALLED_PKGS[@]}" || true
+        fi
+    fi
+
+    # delete build tools
+    rm -rf $RUSTUP_HOME
 
     write_component_version "dynolog" ${DYNOLOG_VERSION}
     write_component_version "dyno_relay_logger" ${DRL_VERSION}
